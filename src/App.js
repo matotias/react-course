1a8b2cfffd5476ec406c93c5977f74096c1fa201
Matias Galleguillos <matotias@gmail.com>
2020-09-27 01:00:35 -0300

    feat: add person component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Matias" age="31"/>
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const person = (props) => (<p>I'm {props.name} and I'm {props.age} years old</p>);
+
+export default person;
