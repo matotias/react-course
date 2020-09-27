d4e36327e878a3311d1787ed442f2fe41827a30d
Matias Galleguillos <matotias@gmail.com>
2020-09-27 01:20:59 -0300

    feat: add onClick handrler to the switch name button

--- src/App.js
+++ src/App.js
@@ -12,18 +12,21 @@ class App extends Component {
         ]
     }
 
-  render() {
-      return (
-          <div className="App">
-              <h1>Hi, I'm a React App</h1>
-              <p>This is really working!</p>
-              <button>Switch Name</button>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is learning Japanese</Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-              <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+    switchNameHandler = () => {
+        console.log("Was clicked!")
+    }
 
-      </div>
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is learning Japanese</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+            </div>
       )
   };
 }
